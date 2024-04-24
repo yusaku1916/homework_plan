980f35a7bf7ab7c7bc61ca7ef18fcffddce438d9
yusaku1916 <hata.yusaku.p1@gmail.com>
2024-04-24 20:49:24 +0900

    2024/4/23_breexe導入

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,39 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WorkController; 
-use App\Http\Controllers\PlanController; 
-use App\Http\Controllers\SubmitController; 
-
+use App\Http\Controllers\WorkController;
+use App\Http\Controllers\PlanController;
+use App\Http\Controllers\SubmitController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 /*Route::get('/', function () {
-    return view('home.screen');
+    return view('welcome');
 });*/
 
-Route::get('/', [WorkController::class, 'index']); 
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+Route::group(['middleware' => ['auth']], function(){
+    Route::get('/', [WorkController::class, 'index'])->name('screen');
+});
 
 Route::get('/works/create', [WorkController::class, 'create']);
 
@@ -33,3 +46,7 @@ Route::post('/plans', [PlanController::class, 'plan_store']);
 Route::get('/submits/create', [SubmitController::class, 'submit_create']);
 
 Route::post('/submits', [SubmitController::class, 'submit_store']);
+    
+});
+
+require __DIR__.'/auth.php';

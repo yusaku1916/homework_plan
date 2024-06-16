7d6cd5fcd4685654cc30ae41c46d3b0126c0c92b
yusaku1916 <hata.yusaku.p1@gmail.com>
2024-06-16 23:27:31 +0900

    [Finish]screenから始まるビューの改善

--- resources/views/home/screen.blade.php
+++ resources/views/home/screen.blade.php
@@ -6,22 +6,15 @@
         <!-- Fonts -->
         <!--<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">-->
         <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('/css/screen_teacher.css') }}">
+        <link rel="stylesheet" href="{{ asset('/css/screen.css') }}">
     </head>
-    <!--x-app-layout-->
+    
     <body>
         <div id="body1">
             
             <header>
                 <h1>宿題連絡帳</h1>
             </header>
-            <!--<nav>-->
-            <!--    <ul class="navi-list">-->
-            <!--        <li class="navi-list-item">1</li>-->
-            <!--        <li class="navi-list-item">1</li>-->
-            <!--        <li class="navi-list-item">1</li>-->
-            <!--    </ul>-->
-            <!--</nav>-->
             
             @if($identify_id == 1)
             <!--先生ビュー-->
@@ -32,20 +25,18 @@
                     </div>
                     
                 @else
-                
+                        
                     <h1 class="HEAD">誰の指導をしますか</h1>
-                    
-                    <form method="POST" action="{{ route("screen.teacher") }}">
+                
+                    <form method="POST" action="{{ route("screen.teacher") }}" id="student_select">
                         @csrf
                         
-                        <div id="student_select">
-                            <select name="student_id">
-                                @foreach($students as $student)
-                                    <option value="{{ $student->id }}">{{ $student->name }}</option>
-                                @endforeach
-                            </select>
-                            <input class="student_select_1" type="submit" value="次へ"/>
-                        </div>
+                        <select name="student_id" id="student_id">
+                            @foreach($students as $student)
+                                <option value="{{ $student->id }}">{{ $student->name }}</option>
+                            @endforeach
+                        </select>
+                        <input type="submit" value="次へ" id="student_select_1"/>
                         
                     </form>
                     
@@ -55,130 +46,130 @@
             <!--生徒ビュー-->
                 
                 <div id='teacher'>
-                    
-                    <div id='work'>
-                        <h2 class="headline">今週の宿題</h2>
-                        @if(is_null($works))
-                            <p class='work_content'>宿題が出るよ</p>
-                        @else
-                            <p class='work_content'>{{ $works->content }}</p>
-                        @endif
+                    <div id="content-coment">
+                        <div id='work'>
+                            <h2 class="headline">今週の宿題</h2>
+                            @if(is_null($works))
+                                <p class='work_content'>宿題が出るよ</p>
+                            @else
+                                <p class='work_content'>{{ $works->content }}</p>
+                            @endif
+                        </div>
+                        
+                        <div id='advice'>
+                            <h2 class="headline">アドバイス・コメント</h2>
+                            @if(is_null($works))
+                                <p class='work_coment'>adviceを待ちましょう</p>
+                            @else
+                                <p class='work_coment'>{{ $works->coment }}</p>
+                            @endif
+                        </div>
                     </div>
-                    
-                    <div id='advice'>
-                        <h2 class="headline">アドバイス・コメント</h2>
-                        @if(is_null($works))
-                            <p class='work_coment'>adviceを待ちましょう</p>
-                        @else
-                            <p class='work_coment'>{{ $works->coment }}</p>
-                        @endif
-                    </div>
-                    
                 </div>
                 
                 <hr>
                 
-                
-                <div id='plans'>
-                    <h2 class="headline">今週の計画</h2>
-                    <button id="plan_button" type=“button” onclick="location.href='/plans/create'"><!-- onclick についてわかっていない -->
-                        計画を立てる
-                    </button>
-                    
-                    @if (is_null($plans1) && is_null($plans2) && is_null($plans3) && is_null($plans4) && is_null($plans5) && is_null($plans6) && is_null($plans7))
-                            
-                        <table border="2">
-                            <tr>
-                                <th>日</th>
-                                <th>月</th>
-                                <th>火</th>
-                                <th>水</th>
-                                <th>木</th>
-                                <th>金</th>
-                                <th>土</th>
-                            </tr>
-                            <tr>
-                                    <td colspan="7"><p>学習予定を立てよう！</p></td>
-                            </tr>
-                        </table>
-                    
-                    @else
-                        <table>
-                            <tr>
-                                <th>日</th>
-                                <th>月</th>
-                                <th>火</th>
-                                <th>水</th>
-                                <th>木</th>
-                                <th>金</th>
-                                <th>土</th>
-                            </tr>
+                <div id="student">
+                    <div id='plans'>
+                        <h2 class="headline">今週の計画</h2>
+                        <button id="plan_button" type=“button” onclick="location.href='/plans/create'"><!-- onclick についてわかっていない -->
+                            計画を立てる
+                        </button>
+                        
+                        @if (is_null($plans1) && is_null($plans2) && is_null($plans3) && is_null($plans4) && is_null($plans5) && is_null($plans6) && is_null($plans7))
+                                
+                            <table border="2">
                                 <tr>
-                                    <td>
-                                        @if(is_null($plans1->content))
-                                        <p>お休み</p>
-                                        @else
-                                        <p>{{ $plans1->content }}</p>
-                                        @endif
-                                    </td>
-                                    <td>
-                                        @if(is_null($plans2->content))
-                                        <p>お休み</p>
-                                        @else
-                                        <p>{{ $plans2->content }}</p>
-                                        @endif
-                                    </td>
-                                    <td>
-                                        @if(is_null($plans3->content))
-                                        <p>お休み</p>
-                                        @else
-                                        <p>{{ $plans3->content }}</p>
-                                        @endif
-                                    </td>
-                                    <td>
-                                        @if(is_null($plans4->content))
-                                        <p>お休み</p>
-                                        @else
-                                        <p>{{ $plans4->content }}</p>
-                                        @endif
-                                    </td>
-                                    <td>
-                                        @if(is_null($plans5->content))
-                                        <p>お休み</p>
-                                        @else
-                                        <p>{{ $plans5->content }}</p>
-                                        @endif
-                                    </td>
-                                    <td>
-                                        @if(is_null($plans6->content))
-                                        <p>お休み</p>
-                                        @else
-                                        <p>{{ $plans6->content }}</p>
-                                        @endif
-                                    </td>
-                                    <td>
-                                        @if(is_null($plans7->content))
-                                        <p>お休み</p>
-                                        @else
-                                        <p>{{ $plans7->content }}</p>
-                                        @endif
-                                    </td>
+                                    <th>日</th>
+                                    <th>月</th>
+                                    <th>火</th>
+                                    <th>水</th>
+                                    <th>木</th>
+                                    <th>金</th>
+                                    <th>土</th>
                                 </tr>
-                        </table>
-                    
-                        <div class="submit">
-                            <button type=“button” onclick="location.href='/submits/create'">
-                                提出
-                            </button>
-                        </div>
-                    @endif
-                    
+                                <tr>
+                                        <td colspan="7"><p>学習予定を立てよう！</p></td>
+                                </tr>
+                            </table>
+                        
+                        @else
+                            <table>
+                                <tr>
+                                    <th>日</th>
+                                    <th>月</th>
+                                    <th>火</th>
+                                    <th>水</th>
+                                    <th>木</th>
+                                    <th>金</th>
+                                    <th>土</th>
+                                </tr>
+                                    <tr>
+                                        <td>
+                                            @if(is_null($plans1->content))
+                                            <p>お休み</p>
+                                            @else
+                                            <p>{{ $plans1->content }}</p>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if(is_null($plans2->content))
+                                            <p>お休み</p>
+                                            @else
+                                            <p>{{ $plans2->content }}</p>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if(is_null($plans3->content))
+                                            <p>お休み</p>
+                                            @else
+                                            <p>{{ $plans3->content }}</p>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if(is_null($plans4->content))
+                                            <p>お休み</p>
+                                            @else
+                                            <p>{{ $plans4->content }}</p>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if(is_null($plans5->content))
+                                            <p>お休み</p>
+                                            @else
+                                            <p>{{ $plans5->content }}</p>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if(is_null($plans6->content))
+                                            <p>お休み</p>
+                                            @else
+                                            <p>{{ $plans6->content }}</p>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if(is_null($plans7->content))
+                                            <p>お休み</p>
+                                            @else
+                                            <p>{{ $plans7->content }}</p>
+                                            @endif
+                                        </td>
+                                    </tr>
+                            </table>
+                        
+                            <div class="submit">
+                                <button type=“button” onclick="location.href='/submits/create'">
+                                    提出
+                                </button>
+                            </div>
+                        @endif
+                        
+                    </div>
                 </div>
+                
             @endif
         </div>
         
-        
-
         <footer>
             <form id="logout" method="POST" action="{{ route('logout') }}">
                 @csrf
@@ -193,5 +184,5 @@
 
 
     </body>
-    <!--/x-app-layout-->
+    
 </html>
